08b9e0122a97263d4842bfa3f1ff5dbd775d276f
Kelly <kelly.li.chang@gmail.com>
2017-10-06 11:48:29 -0400

    2017 portfolio

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,2 +1,15 @@
 $( document ).ready(function() {
+	var scene = document.getElementById('scene');
+	var parallaxInstance = new Parallax(scene);
 });
+
+$(window).resize(function() {
+	console.log('here');
+	var w = window.innerWidth;
+	console.log(w)
+	if (w < 1200) {
+		$(document.getElementById('portrait')).css("left", w *.6 + 'px');
+	}
+
+}
+);
